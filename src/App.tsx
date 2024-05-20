f4e075006bdd2b931b7f361c1393d92370ba2eac
tobicodinghub <hornbogent@helmholtzschule.de>
2024-05-20 21:12:34 +0200

    created base React App

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <div className="flex items-center justify-center min-h-screen bg-gray">
+          <h1 className="text-4x1 font-bold text-blue-600">BOOM SOLUTIONS :)</h1>
+      </div>
+  )
+}
+
+export default App
