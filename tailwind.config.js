3f4528f7767e2b0b3d17abc95ef113d023719b56
tobicodinghub <hornbogent@helmholtzschule.de>
2024-05-28 21:23:42 +0200

    changed configuration for tailwind and prettier, added colour scheme to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF7A00',
+        secondary: {
+          navy: '#003366',
+          cream: '#FFF3E0',
+          teal: '#007D7C'
+        },
+        accent: {
+          charcoal: '#36454F',
+          lightgrey: '#D3D3D3'
+        },
+        darktext: '#333333',
+        lighttext: '#FFFFFF',
+        background: {
+          light: '#FAFAFA',
+          dark: '#282828'
+        },
+        success: '#4CAF50',
+        error: '#F44336',
+        warning: '#FFEB3B',
+        info: '#2196F3'
+      }
+    },
   },
   plugins: [],
-}
+};
 
