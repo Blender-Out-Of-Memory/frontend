74ccc61928513de8ad785c3230bdf247a8e54848
tobicodinghub <hornbogent@helmholtzschule.de>
2024-06-01 13:37:20 +0200

    created screen for registration

---
+++ src/pages/RegistrationScreen.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../components/auth/RegisterForm";
+
+const RegistrationScreen = () => {
+  return (
+    <div className="bg-gray-200 flex items-center justify-center min-h-screen bg-gray">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegistrationScreen;
