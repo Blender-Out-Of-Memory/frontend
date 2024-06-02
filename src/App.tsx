be6cb959e3e15a97a5185b5d66fec54702ed58ef
tobicodinghub <hornbogent@helmholtzschule.de>
2024-06-02 17:39:19 +0200

    added Login to Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistrationScreen from "./pages/RegistrationScreen"
-
+import PrivateRoute from "./contexts/ProtectedRoute"
+import LogInScreen from "./pages/LogInScreen";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
           <Route path="/register" element={<RegistrationScreen/>} />
+          <Route path="/login" element={<LogInScreen/>} />
       </Routes>
     </Router>
       
