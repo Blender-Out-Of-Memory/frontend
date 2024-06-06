1b7686b71dc6f5e8477acec2b6b91e31d0b4a8e4
joooooosef <josef.valentin2004@gmail.com>
2024-06-06 11:40:30 +0200

    changed landig page reroute to actual reroute

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import RegistrationScreen from "./pages/RegistrationScreen"
+import { redirect } from "react-router-dom";
 import PrivateRoute from "./contexts/ProtectedRoute"
 import LogInScreen from "./pages/LogInScreen";
 
@@ -8,7 +9,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<RegistrationScreen/>} />
+          <Route path="/" element={<Navigate to="/register" />} />
           <Route path="/register" element={<RegistrationScreen/>} />
           <Route path="/login" element={<LogInScreen/>} />
       </Routes>
